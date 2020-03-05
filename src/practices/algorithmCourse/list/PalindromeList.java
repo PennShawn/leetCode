39f96af2a29940aae38538840c2958a617082c03
shenpeng <1197921545@qq.com>
2020-03-06 00:05:07 +0800

    add ReverseList

---
+++ src/practices/algorithmCourse/list/PalindromeList.java
@@ -0,0 +1,27 @@
+package practices.algorithmCourse.list;
+
+/**
+ * @Description:
+ * 
+ * @Author: shenpeng
+ * @Date: 2020-03-05
+ */
+public class PalindromeList {
+
+    // Definition for singly-linked list.
+    class ListNode {
+
+        int val;
+
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        int size = 0;
+
+    }
+}
