b68d34eb1f71057239330a0df8e66e5edc95baa8
shenpeng <1197921545@qq.com>
2020-03-12 12:14:37 +0800

    add TrailingZeros

--- src/main/practices/algorithmCourse/SingleNum.java
+++ src/main/practices/algorithmCourse/SingleNum.java
@@ -40,12 +40,13 @@ public class SingleNum {
         }
         return set.iterator().next();
 
-        //        int ans = nums[0];
-        //        if (nums.length > 1) {
-        //            for (int i = 1; i < nums.length; i++) {
-        //                ans = ans ^ nums[i];
-        //            }
-        //        }
-        //        return ans;
+        //                int ans = nums[0];
+        //                if (nums.length > 1) {
+        //                    for (int i = 1; i < nums.length; i++) {
+        //                        ans = ans ^ nums[i];
+        //                    }
+        //                }
+        //                return ans;
     }
+
 }
